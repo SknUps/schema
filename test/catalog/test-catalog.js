58b6443fbf120427c757c3955f9f2641e87843c1
maximilianp <max@sknups.com>
2022-06-28 17:01:52 +0100

    PLATFORM-1853 add catalog schema
    Co-authored-by: Oliver Wilson <oliver.wilson@sknups.com>

---
+++ test/catalog/test-catalog.js
@@ -0,0 +1,12 @@
+const { deserialize } = require('../deserialize')
+const fs = require('fs')
+const path = require('path')
+
+const schema = fs.readFileSync(path.resolve(__dirname, '../../docs/catalog.schema.v1.json'))
+const example = fs.readFileSync(path.resolve(__dirname, 'catalog.json'))
+
+describe('catalog.schema.v1.json', () => {
+  it('validates example JSON', () => {
+    deserialize(example, schema)
+  })
+})
