3fd24acfa4b88d6ca434eb1342f500f44ab61e43
henriquedelgado <32763987+henriquedelgado@users.noreply.github.com>
2022-07-21 15:42:29 +0100

    PLATFORM-2397 publishing new version of catalog JSON schema with discover property (#18)
    PLATFORM-2397 publishing new version of catalog JSON schema with discover property
    
    Co-authored-by: henriquedelgado <henrique@sknups.com>

--- test/catalog/test-catalog.js
+++ test/catalog/test-catalog.js
@@ -2,7 +2,7 @@ const { deserialize } = require('../deserialize')
 const fs = require('fs')
 const path = require('path')
 
-const schema = fs.readFileSync(path.resolve(__dirname, '../../docs/catalog.schema.v1.json'))
+const schema = fs.readFileSync(path.resolve(__dirname, '../../docs/catalog.schema.v2.json'))
 const example = fs.readFileSync(path.resolve(__dirname, 'catalog.json'))
 
 describe('catalog.schema.v1.json', () => {
