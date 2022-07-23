f2ba22cd86852dad8d3267bcde6d9844f33e32ff
Oliver Wilson <88714299+woolinsilver-sknups@users.noreply.github.com>
2022-07-23 19:33:34 -0400

    PLATFORM-2390 publish v1 of brand, platform, collection, giveaway (#20)
    Co-authored-by: Oliver Wilson <oliver.wilson@sknups.com>

--- test/collection/test-collection.js
+++ test/collection/test-collection.js
@@ -2,26 +2,18 @@ const { deserialize } = require('../deserialize')
 const fs = require('fs')
 const path = require('path')
 
-describe('collection.schema.json', () => {
-  it('validates collection of Legacy SKU', () => {
-    const schema = fs.readFileSync(path.resolve(__dirname, '../../docs/collection.schema.json'))
-    const example = fs.readFileSync(path.resolve(__dirname, 'collection-of-legacy-sku.json'))
-    deserialize(example, schema)
-  })
-})
-
 describe('collection-schema-v1.json', () => {
-  it('validates example collection of Legacy SKU', () => {
+  it('validates collection-of-legacy-sku.json', () => {
     const schema = fs.readFileSync(path.resolve(__dirname, '../../docs/collection/collection-schema-v1.json'))
     const example = fs.readFileSync(path.resolve(__dirname, 'collection-of-legacy-sku.json'))
     deserialize(example, schema)
   })
-  it.skip('validates example collection of Giveaway SKU', () => {
+  it('validates collection-of-giveaway-sku.json', () => {
     const schema = fs.readFileSync(path.resolve(__dirname, '../../docs/collection/collection-schema-v1.json'))
     const example = fs.readFileSync(path.resolve(__dirname, 'collection-of-giveaway-sku.json'))
     deserialize(example, schema)
   })
-  it.skip('validates example collection of Premium SKU', () => {
+  it('validates collection-of-premium-sku.json', () => {
     const schema = fs.readFileSync(path.resolve(__dirname, '../../docs/collection/collection-schema-v1.json'))
     const example = fs.readFileSync(path.resolve(__dirname, 'collection-of-premium-sku.json'))
     deserialize(example, schema)
