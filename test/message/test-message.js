21b1dfb9d91ae3dd809794bd047845eec871e054
Oliver Wilson <88714299+woolinsilver-sknups@users.noreply.github.com>
2022-09-26 11:17:36 +0100

    PLATFORM-2712 move message-schema-v1.json to correct location

--- test/message/test-message.js
+++ test/message/test-message.js
@@ -2,7 +2,7 @@ const { deserialize } = require('../deserialize')
 const fs = require('fs')
 const path = require('path')
 
-const schema = fs.readFileSync(path.resolve(__dirname, '../../docs/message.schema.v1.json'))
+const schema = fs.readFileSync(path.resolve(__dirname, '../../docs/message/message-schema-v1.json'))
 const example = fs.readFileSync(path.resolve(__dirname, 'message.json'))
 
 describe('message.schema.v1.json', () => {
