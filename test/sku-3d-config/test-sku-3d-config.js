c43b1f78e45a95f98d75a88f78e601b83d91338c
samliddell-sknups <90681478+samliddell-sknups@users.noreply.github.com>
2022-10-17 11:02:51 +0100

    PLATFORM-2948 update sku-3d-config schema v1, move original version t… (#32)
    PLATFORM-2948 update sku-3d-config schema v1, move original version to v0

--- test/sku-3d-config/test-sku-3d-config.js
+++ test/sku-3d-config/test-sku-3d-config.js
@@ -2,11 +2,25 @@ const { deserialize } = require('../deserialize')
 const fs = require('fs')
 const path = require('path')
 
-const schema = fs.readFileSync(path.resolve(__dirname, '../../docs/sku-3d-config/sku-3d-config-schema-v1.json'))
-const example = fs.readFileSync(path.resolve(__dirname, 'sku.v1.3DConfig.DOLCE-GABBANA-2084-SLOPES-LOOK.json'))
+const versions = {
+  0: {
+    schema: fs.readFileSync(path.resolve(__dirname, '../../docs/sku-3d-config/sku-3d-config-schema-v0.json')),
+    example: fs.readFileSync(path.resolve(__dirname, 'sku-3d-config-v0.json'))
+  },
+  1: {
+    schema: fs.readFileSync(path.resolve(__dirname, '../../docs/sku-3d-config/sku-3d-config-schema-v1.json')),
+    example: fs.readFileSync(path.resolve(__dirname, 'sku-3d-config-v1.json'))
+  }
+}
 
-describe('sku-3d-config-schema-v1.json', () => {
-  it('validates sku.v1.3DConfig.DOLCE-GABBANA-2084-SLOPES-LOOK.json', () => {
+describe('sku-3d-config-schema', () => {
+  it('validates v0', () => {
+    const { schema, example } = versions['0']
+    deserialize(example, schema)
+  })
+
+  it('validates v1', () => {
+    const { schema, example } = versions['1']
     deserialize(example, schema)
   })
 })
