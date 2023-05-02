c987b34b7630fb452847afc976c7ef53ee114c14
Sam Liddell <90681478+samliddell-sknups@users.noreply.github.com>
2023-05-02 15:47:28 +0100

    PLATFORM-3707 update catalog schema to make media mandatory and rename discover to advertise

--- test/catalog-schema-v8/test-catalog.js
+++ test/catalog-schema-v8/test-catalog.js
@@ -9,7 +9,7 @@ const __dirname = dirname(new URL(import.meta.url).pathname);
 const schema = fs.readFileSync(resolve(__dirname, '../../docs/catalog/catalog-schema-v8.json'));
 const example = fs.readFileSync(resolve(__dirname, './catalog.json'));
 
-describe('catalog-schema-v7.json', () => {
+describe('catalog-schema-v8.json', () => {
   it('validates catalog.json', () => {
     deserialize(example, schema);
   });
